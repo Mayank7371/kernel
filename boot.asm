e59dddd58f699ebd8a07e46527bd6b5fa0213eb2
Mayank <166874794+Mayank7371@users.noreply.github.com>
2025-07-30 18:08:33 +0530

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,5 +1,13 @@
-.global _start
-_start:
-   mov r0,#0
-   mov r7,#1
-   swi 0
+ORG 0x7c00
+BITS 16
+ 
+start:
+    mov ah, 0eh
+    mov al, 'A'
+    mov bx, 0
+    int 0x10
+
+    jmp $
+
+    times 510-($-$$) db 0 
+    dw 0xAA55
