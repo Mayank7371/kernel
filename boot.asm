58aed81659fd02c5f6cabc673f12f23e6673fa13
Mayank <166874794+Mayank7371@users.noreply.github.com>
2025-07-31 05:48:25 +0530

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -2,12 +2,12 @@ ORG 0x7c00
 BITS 16
  
 start:
-    mov ah, 0eh
-    mov al, 'A'
+    mov ah, 0eh  ;bios routine for video output
+    mov al, 'A'  ; for printing A
     mov bx, 0
-    int 0x10
+    int 0x10  ; software interrupt
 
     jmp $
 
-    times 510-($-$$) db 0 
+    times 510-($-$$) db 0 ; fills the code with 0 so that total size is 512 bytes since we are making a bootloader
     dw 0xAA55
